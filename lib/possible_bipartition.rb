4b9a2a3dfa0e1f192bd7b4f03ee1c3dee90f9cfb
Dianna Tingg <dtingg@gmail.com>
2020-04-03 21:15:21 -0700

    all except one test passing

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,32 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  group_hash = {}
+
+  visited_hash = {}
+
+  length = dislikes.length
+
+  current_group = true
+
+  length.times do |current_puppy|
+    if !visited_hash[current_puppy]
+      group_hash[current_puppy] = current_group
+      visited_hash[current_puppy] = true
+    end
+
+    queue = dislikes[current_puppy]
+
+    while !queue.empty?
+      check_puppy = queue.pop
+      if visited_hash[check_puppy]
+        return false if group_hash[check_puppy] == group_hash[current_puppy]
+      else
+        group_hash[check_puppy] = !group_hash[current_puppy]
+        visited_hash[check_puppy] = true
+      end
+    end
+    current_group = !current_group
+  end
+
+  return true
 end
