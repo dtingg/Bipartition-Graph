5110fd942469485612591614d05c8c6120cd3165
Dianna Tingg <dtingg@gmail.com>
2020-04-04 18:57:47 -0700

    uncommented code

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -41,40 +41,40 @@ end
 # Breadth First Search
 # Time: O(n + e) where n is the number of nodes in the graph and e is the number of edges.
 # Space: O(n) where n is the number of nodes in the graph.
-# def possible_bipartition(dislikes)
-#   total_puppies = dislikes.length
+def possible_bipartition(dislikes)
+  total_puppies = dislikes.length
 
-#   visited = {}
-#   groups = {}
+  visited = {}
+  groups = {}
 
-#   queue = []
+  queue = []
 
-#   (0...total_puppies).each do |i|
-#     if !visited[i]
-#       queue.push(i)
+  (0...total_puppies).each do |i|
+    if !visited[i]
+      queue.push(i)
 
-#       visited[i] = true
-#       groups[i] = true
-#     end
+      visited[i] = true
+      groups[i] = true
+    end
 
-#     while !queue.empty?
-#       current = queue.shift 
+    while !queue.empty?
+      current = queue.shift 
 
-#       neighbors = dislikes[current]
+      neighbors = dislikes[current]
 
-#       neighbors.each do |puppy|
-#         if !visited[puppy]
-#           queue.push(puppy)
-#           visited[puppy] = true
-#           groups[puppy] = !groups[current]
-#         else
-#           if groups[puppy] == groups[current]
-#             return false
-#           end
-#         end
-#       end
-#     end
-#   end
+      neighbors.each do |puppy|
+        if !visited[puppy]
+          queue.push(puppy)
+          visited[puppy] = true
+          groups[puppy] = !groups[current]
+        else
+          if groups[puppy] == groups[current]
+            return false
+          end
+        end
+      end
+    end
+  end
 
-#   return true
-# end
+  return true
+end
